2c4e0c4246c393b7d72b93f6a5369408bfec1b79
Edilson Rodrigues <edarodrigs@gmail.com>
2021-04-19 20:57:02 -0300

    nlw 1 end

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:dev_quiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,8 +6,9 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "DevQuiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
