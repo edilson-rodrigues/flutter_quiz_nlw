03b688687507d5576eb0316dd074f7f6930d7423
Edilson Rodrigues <edarodrigs@gmail.com>
2021-04-19 18:50:28 -0300

    hello world

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NLW5 Flutter'),
+      ),
+      body: Center(
+        child: Text(
+          'Olá, Edilson DEV',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
 }
