774775baa6de244a47254b9b1f0cfc82d0f014bf
Edilson Rodrigues <edarodrigs@gmail.com>
2021-04-21 08:07:01 -0300

    challenge - question indicator widget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,6 @@
-import 'package:dev_quiz/home/home_page.dart';
-import 'package:dev_quiz/splash/splash_page.dart';
+// TODO import 'package:dev_quiz/home/home_page.dart';
+// TODO import 'package:dev_quiz/splash/splash_page.dart';
+import 'package:dev_quiz/challenge/challenge_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -8,7 +9,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
