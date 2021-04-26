28d3e86322e4a6793aa67c125279ccf13632ef39
Edilson Rodrigues <edarodrigs@gmail.com>
2021-04-26 20:35:57 -0300

    add animation progress

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -1,37 +1,75 @@
-import 'package:dev_quiz/core/app_colors.dart';
-import 'package:dev_quiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class ChartWidget extends StatelessWidget {
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+
+class ChartWidget extends StatefulWidget {
+  final double percent;
+
+  const ChartWidget({
+    Key? key,
+    required this.percent,
+  }) : super(key: key);
+
+  @override
+  _ChartWidgetState createState() => _ChartWidgetState();
+}
+
+class _ChartWidgetState extends State<ChartWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void _initAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 3,
+      ),
+    );
+    _animation =
+        Tween<double>(begin: 0.0, end: widget.percent).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void initState() {
+    _initAnimation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.1,
       width: size.width * 0.1,
-      child: Stack(
-        children: [
-          Center(
-            child: Container(
-              height: size.height * 0.09,
-              width: size.width * 0.2,
-              child: CircularProgressIndicator(
-                strokeWidth: 10,
-                value: .75,
-                backgroundColor: AppColors.chartSecondary,
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  AppColors.chartPrimary,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, _) => Stack(
+          children: [
+            Center(
+              child: Container(
+                height: size.height * 0.09,
+                width: size.width * 0.2,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  value: _animation.value,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    AppColors.chartPrimary,
+                  ),
                 ),
               ),
             ),
-          ),
-          Center(
-            child: Text(
-              '75%',
-              style: AppTextStyles.heading,
+            Center(
+              child: Text(
+                '${(_animation.value * 100).toInt()}%',
+                style: AppTextStyles.heading,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/material.dart';
+
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
 import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
-import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
+  final double percent;
+
+  const ScoreCardWidget({
+    Key? key,
+    required this.percent,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -26,7 +34,9 @@ class ScoreCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: ChartWidget(),
+                child: ChartWidget(
+                  percent: percent,
+                ),
               ),
               Expanded(
                 flex: 3,
